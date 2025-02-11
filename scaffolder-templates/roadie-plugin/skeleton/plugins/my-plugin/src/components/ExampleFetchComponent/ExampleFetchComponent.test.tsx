e688bf7fe42c491a3c57f5e3c98d5e41c86ee76b
João Martins <joao@roadie.io>
2025-02-11 10:40:10 +0000

    Fixing TextEnconder node global updating dependencies

--- scaffolder-templates/roadie-plugin/skeleton/plugins/my-plugin/src/components/ExampleFetchComponent/ExampleFetchComponent.test.tsx
+++ scaffolder-templates/roadie-plugin/skeleton/plugins/my-plugin/src/components/ExampleFetchComponent/ExampleFetchComponent.test.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { ExampleFetchComponent } from './ExampleFetchComponent';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { ExampleFetchComponent } from "./ExampleFetchComponent";
+import { TestApiProvider } from "@backstage/test-utils";
+import { analyticsApiRef } from "@backstage/core-plugin-api";
 
-describe('ExampleFetchComponent', () => {
-  it('renders the user table', async () => {
-    render(<ExampleFetchComponent />);
+const mockAnalyticsApi = {
+  captureEvent: jest.fn(),
+};
+
+describe("ExampleFetchComponent", () => {
+  it("renders the user table", async () => {
+    render(
+      <TestApiProvider apis={[[analyticsApiRef, mockAnalyticsApi]]}>
+        <ExampleFetchComponent />
+      </TestApiProvider>
+    );
 
     // Wait for the table to render
-    const table = await screen.findByRole('table');
-    const nationality = screen.getAllByText("GB")
+    const table = await screen.findByRole("table");
+    const nationality = screen.getAllByText("GB");
+
     // Assert that the table contains the expected user data
     expect(table).toBeInTheDocument();
-    expect(screen.getByAltText('Carolyn')).toBeInTheDocument();
-    expect(screen.getByText('Carolyn Moore')).toBeInTheDocument();
-    expect(screen.getByText('carolyn.moore@example.com')).toBeInTheDocument();
+    expect(screen.getByAltText("Carolyn")).toBeInTheDocument();
+    expect(screen.getByText("Carolyn Moore")).toBeInTheDocument();
+    expect(screen.getByText("carolyn.moore@example.com")).toBeInTheDocument();
     expect(nationality[0]).toBeInTheDocument();
   });
 });
